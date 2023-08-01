8f7b626f3eb67ab999ece1944cf6060db06a4f1e
Jaogatao <playgreen20pg@gmail.com>
2023-07-31 22:11:44 -0300

    primeiros trabalhos com c#

---
+++ Triangulo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Triangulo_C_;
+
+namespace Programa
+{
+    public class Programa
+    {
+        static void Main(string []args){
+            
+            Triangulo x,y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+                Console.WriteLine("Diga as medidas do triângulo X: ");
+
+                x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Diga as medidas do triângulo Y: ");
+                y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    
+                double AreaX = x.Area();
+                double AreaY = y.Area();
+                
+                Console.WriteLine("ÁREA DE X = " + AreaX.ToString("F4", CultureInfo.InvariantCulture));
+                Console.WriteLine("ÁREA DE Y = " + AreaY.ToString("F4", CultureInfo.InvariantCulture));
+}
+    }
+}
